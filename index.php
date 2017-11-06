5eb9c66f3d56476f82e3ef5fd9b51792ef3deff3
Alexander K Lee <akl25@afsaccess1.njit.edu>
2017-11-06 05:08:24 -0500

    Created select function

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 class User {
 
+	var $id;
+	var $fname;
+	var $lname;
+	var $phone;
+	var $birhtday;
+	var $gender;
+	var $password;
+
 	$hostname = "sql2.njit.edu";
 	$username = "akl25";
 	$password = "9TCE4kP41";
@@ -16,6 +24,28 @@ class User {
 		// echo "Connection failed: " . $e->getMessage();
 		http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n" . $e->getMessage().'<br>');
 	}
+
+	// Runs SQL query and returns results (if valid)
+	function runQuery($query) {
+		global $conn;
+	    try {
+			$q = $conn->prepare($query);
+			$q->execute();
+			$results = $q->fetchAll();
+			$q->closeCursor();
+			return $results;	
+		} catch (PDOException $e) {
+			http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n" . $e->getMessage()."<br>");
+		}	  
+	}
+
+	function select() {
+		$sql = "select * from accounts";
+		$results = $this->runQuery($sql);
+		return $results;
+	}
+
+	function insert($id)
 }
 
 // Runs SQL query and returns results (if valid)
