d544dcd0ceaff7a095d9a4dc2fac65ec4df6aa9e
Alexander K Lee <akl25@afsaccess1.njit.edu>
2017-11-07 12:22:33 -0500

    Added constructor

--- index.php
+++ index.php
@@ -10,24 +10,26 @@ class User {
 	var $gender;
 	var $password;
 
-	$hostname = "sql2.njit.edu";
-	$username = "akl25";
-	$password = "9TCE4kP41";
-	$conn = NULL;
-	try 
-	{
-	    $conn = new PDO("mysql:host=$hostname;dbname=akl25",
-	    $username, $password);
-	    echo "<p>Connected successfully<br></p>";
-	}
-	catch(PDOException $e)
-	{
-		// echo "Connection failed: " . $e->getMessage();
-		$this->http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n" . $e->getMessage().'<br>');
+	function __construct() {
+		$hostname = "sql2.njit.edu";
+		$username = "akl25";
+		$password = "9TCE4kP41";
+		$conn = NULL;
+		try 
+		{
+		    $conn = new PDO("mysql:host=$hostname;dbname=akl25",
+		    $username, $password);
+		    echo "<p>Connected successfully<br></p>";
+		}
+		catch(PDOException $e)
+		{
+			// echo "Connection failed: " . $e->getMessage();
+			$this->http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n" . $e->getMessage().'<br>');
+		}
 	}
 
 	// Runs SQL query and returns results (if valid)
-	private function runQuery($query) {
+	function runQuery($query) {
 		global $conn;
 	    try {
 			$q = $conn->prepare($query);
@@ -40,7 +42,7 @@ class User {
 		}	  
 	}
 
-	private function http_error($message) 
+	function http_error($message) 
 	{
 		header("Content-type: text/plain");
 		die($message);
@@ -48,7 +50,7 @@ class User {
 
 	function select() {
 		$sql = "select * from accounts";
-		$results = self::runQuery($sql);
+		$results = $this->runQuery($sql);
 		if(count($results) > 0)
 		{
 			echo "Number of records: ".count($results)."<br><br>";
@@ -73,7 +75,7 @@ class User {
 		$this->$password = $password;
 
 		$sql = "insert into accounts values (".$id.",".$email.",".$fname.",".$lname.",".$phone.",".$birthday.",".$gender.",".$password.")";
-		$results = self::runQuery($sql);
+		$results = $this->runQuery($sql);
 		if(count($results) > 0)
 		{
 			echo "Number of records: ".count($results)."<br><br>";
@@ -91,8 +93,18 @@ class User {
 		$this->$email = $email;
 
 		$sql = "delete from accounts where email=".$email;
-		$results = self::runQuery($sql);
-		return $results;
+		$results = $this->runQuery($sql);
+		if(count($results) > 0)
+		{
+			echo "Number of records: ".count($results)."<br><br>";
+			echo "<table border=\"1\"><tr><th>ID</th><th>Email</th><th>First Name</th><th>Last Name</th><th>Phone</th><th>Birthday</th><th>Gender</th><th>Pass</th></tr>";
+			foreach ($results as $row) {
+				echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["phone"]."</td><td>".$row["birthday"]."</td><td>".$row["gender"]."</td><td>".$row["password"]."</td></tr>";
+			}
+			
+		}else{
+		    echo '0 results';
+		}
 	}
 
 	function update($fname, $lname, $password) {
@@ -101,8 +113,18 @@ class User {
 		$this->$password = $password;
 
 		$sql = "update accounts set password=".$password." where fname=".$fname." and lname=".$lname;
-		$results = self::runQuery($sql);
-		return $results;
+		$results = $this->runQuery($sql);
+		if(count($results) > 0)
+		{
+			echo "Number of records: ".count($results)."<br><br>";
+			echo "<table border=\"1\"><tr><th>ID</th><th>Email</th><th>First Name</th><th>Last Name</th><th>Phone</th><th>Birthday</th><th>Gender</th><th>Pass</th></tr>";
+			foreach ($results as $row) {
+				echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["phone"]."</td><td>".$row["birthday"]."</td><td>".$row["gender"]."</td><td>".$row["password"]."</td></tr>";
+			}
+			
+		}else{
+		    echo '0 results';
+		}
 	}
 }
 
